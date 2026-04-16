8619067344ed44fe26c9d73e1cd12a0eb3a2c84c
drewplaysmusic <drewplaysmusic@gmail.com>
2026-04-16 17:43:32 -0400

    Fix placeholder text color

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div style={{ padding: 24, fontFamily: 'sans-serif' }}>
+    <div style={{ padding: 24, fontFamily: 'sans-serif', color: '#111' }}>
       <h1>PromptScore Prototype</h1>
       <p>Scaffold is working. Full music engine will be added next.</p>
     </div>
