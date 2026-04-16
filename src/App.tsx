31aa502d7a9b27783dcd43f445adef08c9da25f8
drewplaysmusic <drewplaysmusic@gmail.com>
2026-04-16 17:39:00 -0400

    Add placeholder App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: 'sans-serif' }}>
+      <h1>PromptScore Prototype</h1>
+      <p>Scaffold is working. Full music engine will be added next.</p>
+    </div>
+  )
+}
