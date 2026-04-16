e9888c9f23d1658c4013f87c2b005be95d5b0475
drewplaysmusic <drewplaysmusic@gmail.com>
2026-04-16 17:50:21 -0400

    Add background color to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div style={{ padding: 24, fontFamily: 'sans-serif', color: '#111' }}>
+    <div style={{ padding: 24, fontFamily: 'sans-serif', color: '#111', background: '#fff' }}>
       <h1>PromptScore Prototype</h1>
       <p>Scaffold is working. Full music engine will be added next.</p>
     </div>
