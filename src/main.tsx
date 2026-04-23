a8e1fde17dcc94bbc53b825872a8756170770a1a
drewplaysmusic <drewplaysmusic@gmail.com>
2026-04-23 10:23:25 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import PromptScoreShell from './ui/PromptScoreShell'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PromptScoreShell />
   </React.StrictMode>,
 )
