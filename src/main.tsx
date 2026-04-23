d21bcbcfd781de64a6162f098bf3fb3ab4a4301d
drewplaysmusic <drewplaysmusic@gmail.com>
2026-04-23 11:36:06 -0400

    Replace PromptScoreShell with test div

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PromptScoreShell from './ui/PromptScoreShell'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PromptScoreShell />
+    <div style={{ fontSize: 48, fontWeight: 900, color: 'red', padding: 40 }}>
+      MAIN.TSX TEST
+    </div>
   </React.StrictMode>,
 )
