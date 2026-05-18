1b1514c27eca25d78a24bca9f8177dc14fc666ac
drewplaysmusic <drewplaysmusic@gmail.com>
2026-05-17 23:27:02 -0400

    Wire app to harmony shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PromptScoreShell from './ui/PromptScoreShell'
+import PromptScoreShell from './ui/PromptScoreShellHarmony'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
