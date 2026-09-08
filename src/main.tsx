c8fa62d05a5c77f78d643672bd909160d22fce9c
drewplaysmusic <drewplaysmusic@gmail.com>
2026-09-08 11:13:58 -0400

    Use classroom alpha as default app

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PromptScoreShell from './ui/PromptScoreHarmonyWorkbench'
+import PromptScoreClassroomApp from './ui/PromptScoreClassroomApp'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PromptScoreShell />
+    <PromptScoreClassroomApp />
   </React.StrictMode>,
 )
